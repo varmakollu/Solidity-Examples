60269442b56dc7220afc1597d83b154bb3fdde46
Varma Kollu <varmaprince24@yahoo.com>
2024-05-10 18:01:18 +0530

    Update and rename Day-47.md to Event.sol

---
+++ SOLIDITY/Event.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+ pragma solidity 0.8.7;
+ contract Event {
+ 
+  event ProductAdded(uint productID, uint price);
+ 
+  function addProduct(uint _productID, uint _price) public {
+      emit ProductAdded(_productID, _price);
+  }
+ }
