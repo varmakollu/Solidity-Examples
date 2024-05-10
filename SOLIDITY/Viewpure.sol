f6df781d21b477558efe2a0b5a573cab7b557a9d
Varma Kollu <varmaprince24@yahoo.com>
2024-05-10 17:16:12 +0530

    Update and rename Day-41.md to Viewpure.sol

---
+++ SOLIDITY/Viewpure.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+ pragma solidity 0.8.7;
+ 
+ contract ViewPure {
+ 
+  uint x;
+ 
+  function changeVariable() public {
+      x = 100;
+  }
+ 
+  function getVariable() public view returns(uint) {
+      return x;
+  }
+ 
+  function addNumbers() public pure returns(uint) {
+      return (1 + 2);
+  }
+ }
