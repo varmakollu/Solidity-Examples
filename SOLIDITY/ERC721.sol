4bda099194d26689c37795411c9d9a7792d49300
Varma Kollu <varmaprince24@yahoo.com>
2024-06-27 11:13:42 +0530

    Update ERC721.sol

---
+++ SOLIDITY/ERC721.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+
+contract Calyptus is ERC721 {
+   constructor() ERC721("Varma", "VA") {}
+
+
+   function safeMint(address to, uint256 tokenId) public {
+       _safeMint(to, tokenId);
+   }
+}
