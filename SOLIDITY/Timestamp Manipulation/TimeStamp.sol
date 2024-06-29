709a1ddcd0c127167bcb01f27d1ec5c10add0f0d
Varma Kollu <varmaprince24@yahoo.com>
2024-06-29 06:21:50 +0000

    Solidity

---
+++ SOLIDITY/Timestamp Manipulation/TimeStamp.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+contract Roulette {
+   uint public pastBlockTime;
+ 
+   constructor() payable {}
+ 
+   function spin() external payable {
+       require(msg.value == 10 ether); // must send 10 ether to play
+       require(block.timestamp != pastBlockTime); // only 1 transaction per block
+ 
+       pastBlockTime = block.timestamp;
+ 
+       if (block.timestamp % 7 == 0) {
+           (bool sent, ) = 
+     msg.sender.call{value: address(this).balance}("");
+           require(sent, "Failed to send Ether");
+       }
+   }
+}
