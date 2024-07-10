66fd9e19fc367a02e0cfbac6d8f525113e9433eb
Varma Kollu <varmaprince24@yahoo.com>
2024-07-10 04:26:24 +0000

    Solidity

--- SOLIDITY/09-Events/Event.sol
+++ SOLIDITY/09-Events/Event.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: MIT
- pragma solidity 0.8.7;
- contract Event {
- 
-  event ProductAdded(uint productID, uint price);
- 
-  function addProduct(uint _productID, uint _price) public {
-      emit ProductAdded(_productID, _price);
-  }
+
+pragma solidity ^0.8.0;
+
+contract Event {
+    
+    event ProductAdded(uint productID, uint price);
+    
+    function addProduct(uint _productID, uint _price) public {
+        emit ProductAdded(_productID, _price);
+    }
  }
