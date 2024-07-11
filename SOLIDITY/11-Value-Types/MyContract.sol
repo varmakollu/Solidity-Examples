762f0140b5c7d55ea0dd7695391519cd9fefa31c
Varma Kollu <varmaprince24@yahoo.com>
2024-07-11 08:20:00 +0000

    Solidity

--- SOLIDITY/11-Value-Types/MyContract.sol
+++ SOLIDITY/11-Value-Types/MyContract.sol
@@ -4,27 +4,24 @@ pragma solidity ^0.8.0;
 
 contract MyContract {
     bool myBool = true;
-        int8 myInt = -128;
-            uint256 myUint = 123456;
-                address myAddress = 0x1234567890123456789012345678901234567890;
-                    bytes32 myBytes = "Hello, Solidity!";
-                        string myString = "Hello, World!";
+    int8 myInt = -128;
+    uint256 myUint = 123456;
+    address myAddress = 0x1234567890123456789012345678901234567890;
+    bytes32 myBytes = "Hello, Solidity!";
+    string myString = "Hello, World!";
                             
-                                enum Status { Active, Inactive }
-                                    Status myStatus = Status.Active;
+    enum Status { Active, Inactive }
+    Status myStatus = Status.Active;
+    uint[] myDynamicArray;
+    uint[5] myFixedArray;
+    struct Person {
+        string name;
+        uint age;
+    }
 
-                                        uint[] myDynamicArray;
-                                            uint[5] myFixedArray;
-
-                                                struct Person {
-                                                        string name;
-                                                                uint age;
-                                                                    }
-                                                                        Person myPerson = Person("Alice", 30);
-
-                                                                            mapping(address => uint) public balances;
-
-                                                                                function setBalance(address _address, uint _value) public {
-                                                                                        balances[_address] = _value;
-                                                                                            }
-                                                                                            }
+    Person myPerson = Person("Alice", 30);
+    mapping(address => uint) public balances;
+    function setBalance(address _address, uint _value) public {
+    balances[_address] = _value;
+    }
+}
