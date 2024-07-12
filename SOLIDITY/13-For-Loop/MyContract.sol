54f73642d70dcb860ebce5312c7478e265a33fc1
Varma Kollu <varmaprince24@yahoo.com>
2024-07-12 17:15:48 +0000

    Solidity

--- SOLIDITY/13-For-Loop/MyContract.sol
+++ SOLIDITY/13-For-Loop/MyContract.sol
@@ -7,6 +7,7 @@ pragma solidity ^0.8.0;
 contract MyContract {
     uint256 public loopCount;
 
+    // This function performs a loop that increments the loopCount variable n times
     function doLoop(uint256 n) public {
         for (uint256 i = 0; i < n; i++) {
             loopCount += 1;
