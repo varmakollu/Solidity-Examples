ee7b50b740f0c43b71c1a46f7abe8049c429557c
Varma Kollu <varmaprince24@yahoo.com>
2024-07-13 04:37:40 +0000

    Solidity

---
+++ SOLIDITY/15-Mapping/NestedMapsContract.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+// https://cryptomarketpool.com/how-to-create-a-map-in-a-solidity-smart-contract/
+
+pragma solidity ^0.8.0;
+
+contract NestedMapsContract {
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    function get(address _addressOwner, address _addressSpender) public view returns (uint256) {
+        return allowance[_addressOwner][_addressSpender];
+    }
+
+    function set(
+        address _addressOwner,
+        address _addressSpender,
+        uint256 amount
+    ) public {
+        allowance[_addressOwner][_addressSpender] = amount;
+    }
+
+    function remove(address _addressOwner, address _addressSpender) public {
+        delete allowance[_addressOwner][_addressSpender];
+    }
+}
