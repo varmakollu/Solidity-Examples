f1fd71cad2379d8ea103fc1246eb22a58a3d2184
Varma Kollu <varmaprince24@yahoo.com>
2024-07-14 05:38:10 +0000

    Solidity

--- SOLIDITY/16-Enum/Enums.sol
+++ SOLIDITY/16-Enum/Enums.sol
@@ -1,22 +1,25 @@
 // SPDX-License-Identifier: MIT
- pragma solidity 0.8.7;
- 
- contract Enum {
- 
-   enum Status {
-       Ordered,
-       Shipped,
-       Delivered,
-       Cancelled
-   }
- 
-   Status public status;
- 
-   function setStatus(Status _status) public {
-       status = _status;
-   }
- 
-   function getStatus() public view returns(Status) {
-       return status;
-   }
- }
+pragma solidity 0.8.7;
+
+contract Enum {
+    // Define an enumeration with four possible statuses
+    enum Status {
+        Ordered,
+        Shipped,
+        Delivered,
+        Cancelled
+    }
+
+    // State variable to store the current status
+    Status public status;
+
+    // Function to set the status
+    function setStatus(Status _status) public {
+        status = _status;
+    }
+
+    // Function to get the current status
+    function getStatus() public view returns (Status) {
+        return status;
+    }
+}
