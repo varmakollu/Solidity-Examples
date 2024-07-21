7a2b0505b2dd1e4a3f8420184ec09f822a228b4a
Varma Kollu <varmaprince24@yahoo.com>
2024-07-21 09:12:26 +0000

    Solidity

---
+++ SOLIDITY/01_sample_contract/MyContract.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+// https://cryptomarketpool.com/simple-contract
+
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    string private name;
+    uint256 private age;
+
+    function setName(string memory _name) public {
+        name = _name;
+    }
+
+    function getName() public view returns (string memory) {
+        return name;
+    }
+
+    function setAge(uint256 _age) public {
+        age = _age;
+    }
+
+    function getAge() public view returns (uint256) {
+        return age;
+    }
+}
