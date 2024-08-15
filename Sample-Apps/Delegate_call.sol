df130cfb202c631ad1d2ab721942d35c1be1b298
Varma Kollu <varmaprince24@yahoo.com>
2024-08-15 10:17:06 +0000

    Solidity

--- Sample-Apps/Delegate_call.sol
+++ Sample-Apps/Delegate_call.sol
@@ -3,9 +3,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-//Original contract
+// Original contract
 contract Satellite1 {
-    //these state variables need to be in the exact same order of contract A when performing a delegate call
+    // These state variables need to be in the exact same order of contract A when performing a delegate call
     uint public num;
     address public sender;
     uint public value;
@@ -13,7 +13,7 @@ contract Satellite1 {
     constructor() public { owner = msg.sender; }
     address payable owner;
     
-    //capture the following data and save it in the state variables
+    // Capture the following data and save it in the state variables
     function setVars(uint _num) public payable {
         num = _num;
         sender = msg.sender;
@@ -27,9 +27,9 @@ contract Satellite1 {
 
 }
 
-//Now lets say we want to upgrade contract B and we create B2
+// Now lets say we want to upgrade contract B and we create B2
 contract Satellite2 {
-    //these state variables need to be in the exact same order of contract A when performing a delegate call
+    // These state variables need to be in the exact same order of contract A when performing a delegate call
     uint public num;
     address public sender;
     uint public value;
@@ -37,7 +37,7 @@ contract Satellite2 {
     constructor() public { owner = msg.sender; }
     address payable owner;
     
-    //capture the following data and save it in the state variables
+    // Capture the following data and save it in the state variables
     function setVars(uint _num) public payable {
         //lets multiply the num by 2 so we can see a change
         num = 2 * _num;
@@ -45,7 +45,7 @@ contract Satellite2 {
         value = msg.value;
     }
     
-     //send funds back to the owner and destroy the contract
+     // Send funds back to the owner and destroy the contract
     function Destruct() public {
         selfdestruct(owner);
     }
@@ -56,12 +56,12 @@ contract MainContract {
     address public sender;
     uint public value;
 
-    //this is a delegate call to contract B
-    //we are going to send ether to contract so we are making it payable
+    // This is a delegate call to contract B
+    // We are going to send ether to contract so we are making it payable
     function setVars(address _contract, uint _num) public payable {
         
-        //this is to make a delegate call to another contract
-        //the delegate call will produce 2 outputs.  success if there are no errors and the output of the function in bytes 
+    // This is to make a delegate call to another contract
+    // The delegate call will produce 2 outputs.  success if there are no errors and the output of the function in bytes 
       (bool success, bytes memory data) = _contract.delegatecall(
             
             //in abi sig we need to pass in the function signature that we are calling
