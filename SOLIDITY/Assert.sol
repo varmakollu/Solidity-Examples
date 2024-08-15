7f0bb7b1eff270286251002f549bfee68640e2a7
Varma Kollu <varmaprince24@yahoo.com>
2024-08-15 10:04:06 +0000

    Solidity

--- SOLIDITY/Assert.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract AssertExample {
-    function example(uint256 x) external pure returns (uint256) {
-        assert(x != 0);
-        return 100 / x;
-    }
-}
