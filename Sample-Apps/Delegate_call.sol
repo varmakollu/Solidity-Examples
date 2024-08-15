b09a86d0e7b38f13a3a58cadae251c285c618980
Varma Kollu <varmaprince24@yahoo.com>
2024-08-15 10:13:43 +0000

    Solidity

---
+++ Sample-Apps/Delegate_call.sol
@@ -0,0 +1,29 @@
+// To make this work I needed to use a lot of gas in the test environment or it will fail
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+//Original contract
+contract Satellite1 {
+    //these state variables need to be in the exact same order of contract A when performing a delegate call
+    uint public num;
+    address public sender;
+    uint public value;
+    
+    constructor() public { owner = msg.sender; }
+    address payable owner;
+    
+    //capture the following data and save it in the state variables
+    function setVars(uint _num) public payable {
+        num = _num;
+        sender = msg.sender;
+        value = msg.value;
+    }
+    
+    //send funds back to the owner and destroy the contract
+    function Destruct() public {
+        selfdestruct(owner);
+    }
+    
+
+}
